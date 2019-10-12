9c60100397a41acc36cdbec69e64a61ef8a5687e
styledigger <32388656+styledigger@users.noreply.github.com>
2019-10-12 07:53:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var cities = {cities:["Bratislava","Berlin","New York","San Francisco","Tokyo"]}
+var cities = {cities:["Nitra","Berlin","New York","San Francisco","Tokyo"]}
 
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
